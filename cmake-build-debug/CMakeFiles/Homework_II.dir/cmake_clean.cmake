986a8a04c8a49312740ff9f6e3852529c7ac33b2
makifozkanoglu <makifozkanoglu@gmail.com>
2019-05-12 22:10:40 +0300

    License Readme

---
+++ cmake-build-debug/CMakeFiles/Homework_II.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework_II.dir/main.cpp.o"
+  "Homework_II.pdb"
+  "Homework_II"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework_II.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
